5a17bb8c8e9c5d0e59f7ee164d6db68f52fb17e5
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2022-12-15 14:32:24 +0900

    Update main_javaf.js

--- js/main_javaf.js
+++ js/main_javaf.js
@@ -2,7 +2,7 @@ var data = [];
 
 var table = document.getElementById('table');
 Input_from_gas();
-var judge = [];
+
 function search(e)
 {
 	var keyword = e.value;
@@ -10,22 +10,21 @@ function search(e)
 	var results = [];
 	results[0] = data[0];
 	
-	
+	var judge = [];
 	for(var i = 1; i < data.length; i++)
 	{
 		judge[1] = String(data[i][1]).indexOf(keyword)
 		judge[2] = String(data[i][2]).indexOf(keyword)
-		alert(String(judge[1]) + String(judge[2]));
 		if(judge[1] !== -1 || judge[2] !== -1){results.push(data[i]);}
 	}
 	
 	table.innerHTML = '';
-	for(var i = 0; i < data.length; i++)	//データ出力
+	for(var i = 0; i < results.length; i++)	//データ出力
 	{
 		var row = table.insertRow(-1);
-		for(var j = 0; j < data[i].length; j++)
+		for(var j = 0; j < results[i].length; j++)
 		{
-			var obj = '<td nowrap>' + data[i][j] + '</td>';
+			var obj = '<td nowrap>' + results[i][j] + '</td>';
 			row.insertAdjacentHTML('beforeend',obj);
 		}
 	}
