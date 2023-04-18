da69e022a5787a676af5e34af5293949d37e4980
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2023-04-18 16:16:27 +0900

    Update photo_javaf.js

--- js/photo_javaf.js
+++ js/photo_javaf.js
@@ -1,1 +1,19 @@
+var bg = document.getElementsByClassName('bg');
+
+bg.style.backgroundColor = "rgba(255,255,255,0.5)";
+bg.style.position = 'fixed';
+bg.style.top = '0px';
+bg.style.left = '0px';
+function photo_close()
+{
+
+  bg.clientWidth = '0px';
+  bg.clientHeight = '0px';
+}
+
+function photo_open()
+{
+  bg.clientWidth = window.innerWidth + 'px';
+  bg.clientHeight = window.innerHeight + 'px';
+}
 
