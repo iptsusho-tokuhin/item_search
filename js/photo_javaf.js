48565e0d3435f5a1dc7e528a28943e49021cca1b
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2023-04-19 15:45:17 +0900

    Update photo_javaf.js

--- js/photo_javaf.js
+++ js/photo_javaf.js
@@ -1,5 +1,8 @@
 var bg = document.getElementById('bg');
 var img = document.getElementById('img');
+var img_test = document.getElementById('img_test');
+
+img_test.src = f_list[0][2];
 
 bg.style.backgroundColor = 'rgba(0,0,0,0.7)';
 bg.style.position = 'fixed';
@@ -27,7 +30,16 @@ function get_id(num)
 {
   for(var i = 0; i < f_list.length; i++)
   {
-    if(!f_list[0].indexOf(num)){return i;}// 前方一致のときの処理
+    if(!f_list[i][0].indexOf(num)){return f_list[i][1];}// 前方一致のときの処理
+  }
+  return false;
+}
+
+function get_url(num)
+{
+  for(var i = 0; i < f_list.length; i++)
+  {
+    if(!f_list[i][0].indexOf(num)){return f_list[i][2];}// 前方一致のときの処理
   }
   return false;
 }
