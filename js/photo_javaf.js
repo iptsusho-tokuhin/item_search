cc363788b8f20ee30b5ddf310ec912f649afb375
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2023-04-19 16:14:51 +0900

    Update photo_javaf.js

--- js/photo_javaf.js
+++ js/photo_javaf.js
@@ -1,8 +1,5 @@
 var bg = document.getElementById('bg');
 var img = document.getElementById('img');
-var img_test = document.getElementById('img_test');
-
-img_test.src = f_list[0][2];
 
 bg.style.backgroundColor = 'rgba(0,0,0,0.7)';
 bg.style.position = 'fixed';
@@ -19,11 +16,7 @@ function photo_open(num)
   bg.style.width = window.innerWidth + 'px';
   bg.style.height = window.innerHeight + 'px';
   
-  var id = get_id(num);
-  //if(id)
-  //{
-    img.src = 'https://drive.google.com/uc?id=' + id;
-  //}
+  input_to_img(get_id(num));
 }
 
 function get_id(num)
@@ -31,15 +24,6 @@ function get_id(num)
   for(var i = 0; i < f_list.length; i++)
   {
     if(!f_list[i][0].indexOf(num)){return f_list[i][1];}// 前方一致のときの処理
-  }
-  return false;
-}
-
-function get_url(num)
-{
-  for(var i = 0; i < f_list.length; i++)
-  {
-    if(!f_list[i][0].indexOf(num)){return f_list[i][2];}// 前方一致のときの処理
   }
   return false;
 }
