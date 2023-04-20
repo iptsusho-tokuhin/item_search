92656c5d114aac34eee34eecf91d42da7c248a03
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2023-04-20 16:01:42 +0900

    Update photo_javaf.js

--- js/photo_javaf.js
+++ js/photo_javaf.js
@@ -1,22 +1,25 @@
-var bg = document.getElementById('bg');
+var viewer = document.getElementById('viewer');
 var img = document.getElementById('img');
 
-bg.style.backgroundColor = 'rgba(0,0,0,0.7)';
-bg.style.position = 'fixed';
-bg.style.top = '0px';
-bg.style.left = '0px';
+viewer.style.backgroundColor = 'rgba(0,0,0,0.7)';
+viewer.style.position = 'fixed';
+viewer.style.display ="none";
+viewer.style.top = '0px';
+viewer.style.left = '0px';
+viewer.style.width = window.innerWidth + 'px';
+viewer.style.height = window.innerHeight + 'px';
 function photo_close()
 {
-  bg.style.width = '0px';
-  bg.style.height = '0px';
+  //viewer.style.width = '0px';
+  //viewer.style.height = '0px';
   img.style.height = '0px';
   img.src = "";
 }
 
 function photo_open(num)
 {
-  bg.style.width = window.innerWidth + 'px';
-  bg.style.height = window.innerHeight + 'px';
+  //viewer.style.width = window.innerWidth + 'px';
+  //viewer.style.height = window.innerHeight + 'px';
   var h = window.innerHeight * 0.6;
   img.style.height = h + 'px';
   input_to_img(get_id(num));
