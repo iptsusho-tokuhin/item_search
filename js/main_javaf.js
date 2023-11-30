77aec02673e1410e89ef0a41f7fc0694262e1cab
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2023-11-30 09:03:43 +0900

    Update main_javaf.js

--- js/main_javaf.js
+++ js/main_javaf.js
@@ -35,6 +35,8 @@ function search(e)
 			row.insertAdjacentHTML('beforeend',obj);
 		}
 	}
-	table.cells[0].style.position = 'sticky';//1列目を固定
+	table.rows[0].cells[0].style.position = 'sticky';//1列目を固定
+	table.rows[1].cells[0].style.position = 'sticky';//1列目を固定
+	table.rows[2].cells[0].style.position = 'sticky';//1列目を固定
 }
 
